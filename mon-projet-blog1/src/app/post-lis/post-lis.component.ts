e60b61ccd3a144672d0822e7e6cc0549de2633a2
unknown <ldamaro98@gmail.com>
2018-07-21 08:27:57 +0200

    création de  component Post-lis

---
+++ mon-projet-blog1/src/app/post-lis/post-lis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-lis',
+  templateUrl: './post-lis.component.html',
+  styleUrls: ['./post-lis.component.scss']
+})
+export class PostLisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
