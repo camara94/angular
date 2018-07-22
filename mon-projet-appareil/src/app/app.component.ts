0a065700ebb96d027beea6717f52b2bad8afa212
unknown <ldamaro98@gmail.com>
2018-07-22 06:27:41 +0200

    creation des services à l'application

--- mon-projet-appareil/src/app/app.component.ts
+++ mon-projet-appareil/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { AppareilService } from './services/appareil.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  isAuth = false;
+  appareils:any[];
+  constructor(private appareilService:AppareilService){
+    setTimeout(()=>{
+      this.isAuth = true;
+    },4000);
+  }
+
+  ngOnInit(){
+    this.appareils = this.appareilService.appareils;
+  }
+
+  onSwitchOnAll(){
+    this.appareilService.switchOnAll()
+  }
+
+  onSwitchOffAll(){
+    this.appareilService.switchOffAll();
+  }
 }
--- mon-projet-appareil/src/app/app.module.ts
+++ mon-projet-appareil/src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppareilComponent } from './appareil/appareil.component';
+import { FormsModule} from '@angular/forms';
+import { AppareilService } from './services/appareil.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppareilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AppareilService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
