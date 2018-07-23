461bcff3ae36b227b90d3863f158d62829be5cf3
unknown <ldamaro98@gmail.com>
2018-07-23 02:28:26 +0200

    Création des components " auth et appareil-view "

--- mon-projet-appareil/src/app/app.module.ts
+++ mon-projet-appareil/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { AppareilComponent } from './appareil/appareil.component';
 import { FormsModule} from '@angular/forms';
 import { AppareilService } from './services/appareil.service';
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppareilComponent
+    AppareilComponent,
+    AuthComponent,
+    AppareilViewComponent
   ],
   imports: [
     BrowserModule,
