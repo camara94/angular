6f4bdc5357e90fbed15875c1b36607c719068428
unknown <ldamaro98@gmail.com>
2018-07-24 04:36:54 +0200

    Création des component auth et single-appareil et leur implementation

---
+++ mon-projet-appareil/src/app/services/auth.service.ts
@@ -0,0 +1,22 @@
+import {reject} from 'q';
+
+export class AuthService {
+
+  isAuth = false;
+
+  signIn(){
+    return new Promise(
+      (resolve,reject)=>{
+        setTimeout(()=>{
+          this.isAuth = true;
+          resolve(true);
+        },2000);
+      }
+    );
+  }
+
+  signOut(){
+    this.isAuth = false;
+  }
+
+}
